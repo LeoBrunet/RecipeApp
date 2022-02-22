a8cb7d8404b665d64ed600f9a940918bbec5d29d
Leo Brunet <leobrunet91@gmail.com>
2022-02-22 08:20:04 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by m1 on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
